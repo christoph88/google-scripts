766ed2fc1b2acea848d549bf2de1efcb66db5acb
Christoph Geypen <geypenchristoph@gmail.com>
2017-06-08 13:15:42 +0200

    cleanup working code

--- gapAPI.js
+++ gapAPI.js
@@ -1,11 +1,54 @@
-function onOpen(e) {
-  SpreadsheetApp.getUi()
-    .createMenu('Google Analytics')
-    .addItem('Get statistics', 'readAndWriteRows')
-    .addToUi();
+var spreadsheet = SpreadsheetApp.getActiveSpreadsheet(); 
+
+function metrics() {
+  var metrics = 'ga:newUsers, ga:percentNewSessions, ga:sessions, ga:bounceRate, ga:avgSessionDuration, ga:pageValue, ga:pageviews, ga:timeOnPage, ga:exits';
+
+  return metrics
 }
 
-var spreadsheet = SpreadsheetApp.getActiveSpreadsheet(); 
+function getGAPdata(pagepath) {
+  //var today = new Date();
+  //var oneWeekAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+  //var startDate = Utilities.formatDate(oneWeekAgo, Session.getTimeZone(),'yyyy-MM-dd');
+  //var endDate = Utilities.formatDate(today, Session.getTimeZone(),'yyyy-MM-dd');
+
+  var startDate = '7daysAgo';
+  var endDate = 'today';
+  var profileId = '109751388';
+  var tableId  = 'ga:' + profileId;
+  var metric = metrics();
+  var options = {
+    'dimensions': 'ga:pagePath',
+    'filters': 'ga:pagePath=~' + pagepath
+  };
+  var report = Analytics.Data.Ga.get(tableId, startDate, endDate, metric,
+    options);
+
+  var output = report.totalsForAllResults;
+
+  var result = [];
+
+  for (var key in output) {
+    if (output.hasOwnProperty(key)) {
+      result.push(output[key]);
+    }
+  }
+
+  return result;
+
+}
+
+function processRows(row, URLcolNumber) {
+
+  // replace website name in searchable url
+  var websites = new RegExp('http://www.(centerparcs.be|sunparks|pierreetvacances).(com|be|fr|de|nl)','gi');
+  var url = row[URLcolNumber].replace(websites,'');
+
+  var result = [getGAPdata(url)];
+
+  return result;
+
+};
 
 /**
  * Retrieves all the rows in the defined spreadsheet that contain data and logs the
@@ -17,12 +60,12 @@ function readAndWriteRows() {
 
   var sheetName = spreadsheet.getRangeByName('sheetName').getValues();
   var sheet = spreadsheet.getSheetByName(sheetName);
-  var header = [['Newusers','Percentnewsessions','Sessions','Bouncerate','Avgsessionduration','Pagevalue','Pageviews','Timeonpage','Exits']];
-  
+  var header = [metrics().replace(/ga:/g,'').split(',')];
+
   var rows = sheet.getDataRange();
   var numRows = rows.getNumRows();
   var values = rows.getValues();
-  
+
   // setup colNumber which contains the url in the row output
   var URLcolNumber = spreadsheet.getRangeByName('URLcolNumber').getValues()-1; // -1 because here we are working on an array
   var destColNumber = spreadsheet.getRangeByName('destColNumber').getValues();
@@ -31,11 +74,11 @@ function readAndWriteRows() {
 
   for (var i = 0; i <= numRows - 1; i++) {
     var row = values[i];
-    
+
     // setup the row where the values need to be set
     var destRowNumber = i;
-   
-    
+
+
     // push api data to the correct range
     // getRange(row, column, numRows, numColumns)
 
@@ -48,32 +91,4 @@ function readAndWriteRows() {
     }
 
   }
-};
-
-function processRows(row, URLcolNumber) {
-  Logger.log(URLcolNumber);
-  Logger.log(row);
-
-  // api setup 
-  var accessToken = spreadsheet.getRangeByName('accessToken').getValues();
-
-  // replace website name in searchable url
-  var websites = new RegExp('http://www.(centerparcs.be|sunparks|pierreetvacances).(com|be|fr|de|nl)','gi');
-  var url = row[URLcolNumber].replace(websites,'');
-
-  var apiEndpoint = 'https://www.googleapis.com/analytics/v3/data/ga?'
-    +'ids=ga%3A109751388'
-    +'&start-date=7daysAgo'
-    +'&end-date=yesterday' 
-    +'&metrics=ga%3AnewUsers%2Cga%3ApercentNewSessions%2Cga%3Asessions%2Cga%3AbounceRate%2Cga%3AavgSessionDuration%2Cga%3ApageValue%2Cga%3Apageviews%2Cga%3AtimeOnPage%2Cga%3Aexits'
-    +'&dimensions=ga%3ApagePath'
-    +'&sort=-ga%3Asessions'
-    +'&filters=ga%3ApagePath%3D~'+url 
-    +'&access_token='+accessToken 
-    +'&prettyPrint=true'
-
-  var result = ImportJSON(apiEndpoint, "/totalsForAllResults", 'noHeaders');
-
-  return result;
-
 };
